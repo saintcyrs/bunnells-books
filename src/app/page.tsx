bc052fbb2b09a1d158212b62d82a63a0d7593663
mabunnell3163 <mabunnell3163@gmail.com>
2025-07-13 15:19:13 -0400

    removed title header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import BooksTable from "./page-table";
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center bg-gray-50 p-3 sm:p-0">
-      <h1 className="text-2xl sm:text-4xl font-bold mb-4 sm:mb-6 text-center">Bunnell&apos;s Books</h1>
       <BooksTable />
     </main>
   );
