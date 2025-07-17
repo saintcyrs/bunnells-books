d57b500910eaac50c8d8ee990b53416f3b8e1173
Soleil Saint-Cyr <ssaintcyr@college.harvard.edu>
2025-07-17 19:56:19 -0400

    fixing covers

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         pathname: '/storage/v1/object/public/**',
       },
       {
+        protocol: 'https',
+        hostname: 'grlqndeagtqajqbdcqxz.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
         protocol: "https",
         hostname: "covers.openlibrary.org",
       }
